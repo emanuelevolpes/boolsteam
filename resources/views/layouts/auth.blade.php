e04bff85fafd510bbd943bb4b877bc0ebb95d3cf
Luca Zanfrisco <lzanfrisco9@gmail.com>
2023-05-24 17:37:17 +0200

    Header nav bar

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -46,6 +46,14 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                             </li>
+                            @auth
+                            <li class="nav-item">
+                                <a class="nav-link" style="{{ Route::currentRouteName() === 'admin.dashboard' ? 'color:red; text-decoration:underline ': ''}}" href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" style="{{ Route::currentRouteName() === 'admin.games.index' ? 'color:red; text-decoration:underline ': ''}}" href="{{ route('admin.games.index') }}">{{ __('Games') }}</a>
+                            </li>
+                            @endauth
                         </ul>
 
                         <!-- Right Side Of Navbar -->
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,2 @@
 <footer>
-    <h2>
-        FOOTER PLACEHOLDER
-    </h2>
 </footer>
